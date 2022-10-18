e2d4c4221399778ff39ea42230e81619fe27d814
sbhoora <shubhibhoora@gmail.com>
2022-10-18 02:34:09 -0400

    Send user info to database

---
+++ app/app/src/main/java/com/example/app/Account.java
@@ -0,0 +1,11 @@
+package com.example.app;
+
+public interface Account {
+
+    String getFirstName();
+    String getLastName();
+    String getEmail();
+    String getPassword();
+    String getAccountType();
+
+}
---
+++ app/app/src/main/java/com/example/app/Cook.java
@@ -0,0 +1,44 @@
+package com.example.app;
+
+public class Cook implements Account {
+    String firstName;
+    String lastName;
+    String email;
+    String password;
+    String description;
+
+    public Cook(String f, String l, String e, String p, String d) 
+    {
+        firstName = f;
+        lastName = l;
+        email = e;
+        password = p;
+        description = d;
+        
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public String getAccountType(){
+        return "Cook";
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+}
