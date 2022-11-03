f069b9ad7a6d8426472ae039e8602c1df7d0b1b2
ultra-passion-fruit <matsuru@hoshi.com.br>
2022-11-03 17:09:00 -0400

    class name is singular

--- app/app/src/main/java/com/example/app/Complaint.java
+++ app/app/src/main/java/com/example/app/Complaint.java
@@ -1,12 +1,12 @@
 package com.example.app;
 
-public class Complaints {
+public class Complaint {
     private String subject;
     private String complaintAbout; // either the beginning of the email or the entire thing??
     private String email;
     private String complaint;
 
-    public Complaints(String sub, String aUser ,String mail, String comp) {
+    public Complaint(String sub, String aUser , String mail, String comp) {
         this.subject = sub;
         this.complaintAbout = aUser;
         this.email = mail;
