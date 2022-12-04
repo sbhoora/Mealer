1da298bfeeb468836ed392b8d0dd7047e91b8d44
ultra-passion-fruit <matsuru@hoshi.com.br>
2022-12-04 11:33:24 -0500

    added nav for cook

---
+++ app/app/src/main/java/com/example/app/NewCookHome.java
@@ -0,0 +1,14 @@
+package com.example.app;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class NewCookHome extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_new_cook_home);
+    }
+}
