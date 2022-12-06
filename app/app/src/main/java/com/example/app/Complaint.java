d6b6ff7f1ed327df261a9f39189aa53a8f255373
ultra-passion-fruit <matsuru@hoshi.com.br>
2022-12-06 09:22:53 -0500

    string name change

--- app/app/src/main/java/com/example/app/Complaint.java
+++ app/app/src/main/java/com/example/app/Complaint.java
@@ -3,16 +3,16 @@ package com.example.app;
 public class Complaint {
     private String subject;
     private String cookEmail; // either the beginning of the email or the entire thing??
-    private String complaint;
+    private String description;
 
     public Complaint(String sub, String aUser , String comp) {
         this.subject = sub;
         this.cookEmail = aUser;
-        this.complaint = comp;
+        this.description = comp;
     }
 
     public String getDescription() {
-        return complaint;
+        return description;
     }
 
     public String getCookEmail() {
