cfdb314e0e7c4d9ba0cb859bbf0bda1a139f9d65
ultra-passion-fruit <matsuru@hoshi.com.br>
2022-12-07 16:26:51 -0500

    complaint fragment completed, need connecting

--- app/app/src/main/java/com/example/app/ClientComplaintFragment.java
+++ app/app/src/main/java/com/example/app/ClientComplaintFragment.java
@@ -2,12 +2,20 @@ package com.example.app;
 
 import android.os.Bundle;
 
+import androidx.annotation.NonNull;
 import androidx.fragment.app.Fragment;
 
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.Toast;
 
+import com.google.android.gms.tasks.OnFailureListener;
+import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.android.material.button.MaterialButton;
 import com.google.firebase.database.FirebaseDatabase;
 
 /**
@@ -19,14 +27,13 @@ public class ClientComplaintFragment extends Fragment {
 
     View view;
 
-    // TODO: Rename parameter arguments, choose names that match
-    // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-    private static final String ARG_PARAM1 = "param1";
-    private static final String ARG_PARAM2 = "param2";
+    // Firebase database reference
+    private FirebaseDatabase database = FirebaseDatabase.getInstance();
 
-    // TODO: Rename and change types of parameters
-    private String mParam1;
-    private String mParam2;
+    // the fragment initialization parameter
+    private static final String EMAIL = "email";
+    // the string instance variable storing the email
+    private String email;
 
     public ClientComplaintFragment() {
         // Required empty public constructor
@@ -36,16 +43,14 @@ public class ClientComplaintFragment extends Fragment {
      * Use this factory method to create a new instance of
      * this fragment using the provided parameters.
      *
-     * @param param1 Parameter 1.
-     * @param param2 Parameter 2.
+     * @param email Cook Email.
      * @return A new instance of fragment ClientComplaintFragment.
      */
     // TODO: Rename and change types and number of parameters
-    public static ClientComplaintFragment newInstance(String param1, String param2) {
+    public static ClientComplaintFragment newInstance(String email) {
         ClientComplaintFragment fragment = new ClientComplaintFragment();
         Bundle args = new Bundle();
-        args.putString(ARG_PARAM1, param1);
-        args.putString(ARG_PARAM2, param2);
+        args.putString(EMAIL, email);
         fragment.setArguments(args);
         return fragment;
     }
@@ -54,8 +59,7 @@ public class ClientComplaintFragment extends Fragment {
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         if (getArguments() != null) {
-            mParam1 = getArguments().getString(ARG_PARAM1);
-            mParam2 = getArguments().getString(ARG_PARAM2);
+            email = getArguments().getString(EMAIL);
         }
     }
 
@@ -64,6 +68,62 @@ public class ClientComplaintFragment extends Fragment {
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
         view = inflater.inflate(R.layout.fragment_client_complaint, container, false);
+
+        // Distinguishing emails
+        String cookEmailFromActivity = email;
+
+        // TextView from XML
+        TextView cookEmail = (TextView) view.findViewById(R.id.clientComplaintCookEmail);
+        EditText subject = (EditText) view.findViewById(R.id.clientComplaintSubject);
+        EditText description = (EditText) view.findViewById(R.id.clientComplaintDescriptionTextView);
+
+        // Buttons from XML
+        MaterialButton submitButton = (MaterialButton) view.findViewById(R.id.clientComplaintSubmitComplaintButton);
+        MaterialButton cancelButton = (MaterialButton) view.findViewById(R.id.clientComplaintCancelComplaintButton);
+
+        // Setting cook email
+        cookEmail.setText(email);
+
+        // TextViews as strings
+        String subjectAsString = subject.getText().toString();
+        String descriptionAsString = description.getText().toString();
+
+        // Checking that the user has provided all information and nothing is blank
+        if (!subjectAsString.isEmpty() && !descriptionAsString.isEmpty()) {
+            // Listening for client to click on submit button
+            submitButton.setOnClickListener(new View.OnClickListener() {
+                Complaint complaint = new Complaint(subjectAsString, cookEmailFromActivity, descriptionAsString);
+                @Override
+                public void onClick(View v) {
+                    database.getReference("Accounts").child("admin").child("Complaints").setValue(complaint)
+                            .addOnSuccessListener(new OnSuccessListener<Void>() {
+                                @Override
+                                public void onSuccess(Void unused) {
+                                    Log.i("Firebase", "Complaint saved successfully against cook: " + cookEmailFromActivity);
+                                }
+                            })
+                            .addOnFailureListener(new OnFailureListener() {
+                                @Override
+                                public void onFailure(@NonNull Exception e) {
+                                    Log.e("Firebase", "Failed to save complaint against cook:" + cookEmailFromActivity);
+                                }
+                            });
+                }
+            });
+        } else {
+            // Put red flags
+            subject.setError("Please fill in this field.");
+            description.setError("Please fill in this field.");
+            // Sending out toast message notifying client to fill in fields
+            Toast.makeText(getContext(),"Please fill in all the fields.", Toast.LENGTH_SHORT).show();
+        }
+        cancelButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+            }
+        });
+
         return view;
     }
 
